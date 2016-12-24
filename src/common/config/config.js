81904969b00adaa109d7395c34aeea98bf18e5db
MartinHuangJing <253887111@qq.com>
2016-12-24 21:37:49 +0800

    页面提交

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -3,5 +3,5 @@
  * config
  */
 export default {
-  //key: value
-};
+  port: 8361 //将监听的端口修改为 1234
+}
--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -11,4 +11,24 @@ export default class extends Base {
     //auto render template file index_index.html
     return this.display();
   }
+  async historyAction(){
+    //auto render template file index_history.html
+    let result = await this.model('history').order("year ASC,month ASC").select();
+    this.assign('list',result);
+    return this.display();
+  }
+  cultureAction(){
+    //auto render template file index_culture.html
+    return this.display();
+  }
+  infoAction(){
+    //auto render template file index_info.html
+    return this.display();
+  }
+  async teamAction(){
+    //auto render template file index_team.html
+    let result = await this.model('team').order("num ASC").select();
+    this.assign('list',result);
+    return this.display();
+  }
 }
